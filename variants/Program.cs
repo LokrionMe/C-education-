2776818cc6b73a44fc6537e465522388255e1b17
Lokrion <m.kostian@yandex.ru>
2022-08-18 21:58:56 +0700

    add variants

---
+++ variants/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Who r u? ");
+string username = Console.ReadLine();
+
+if (username == "Lokrion")
+{
+    Console.WriteLine("Wow it's u");
+}
+else
+{
+    Console.Write("So, hi, ");
+    Console.WriteLine(username);
+}
