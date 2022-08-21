d33cb4947922b3d1635441e2093e7ac107dd98c9
Lokrion <m.kostian@yandex.ru>
2022-08-21 11:25:09 +0700

    chs

---
+++ Lesson 1/variants/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Who r u? ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "lokrion")
+{
+    Console.WriteLine("Wow it's u");
+}
+else
+{
+    Console.Write("So, hi, ");
+    Console.WriteLine(username);
+}
